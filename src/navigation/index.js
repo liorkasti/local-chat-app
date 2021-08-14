2199e663cd55e6d5e6b3f8086097b8be803e7709
Lior Kastenbaum <liorkasti@gmail.com>
2021-08-14 23:03:17 +0300

    Add react-native-elements

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -1,8 +1,6 @@
 import React, { useState, useEffect, useReducer, useMemo } from 'react';
-import { View, ActivityIndicator, StatusBar } from 'react-native';
-import { NavigationContainer, /* createStackNavigator */ } from '@react-navigation/native';
+import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import { NavigationActions } from 'react-navigation';
 import { useSelector } from 'react-redux';
 
 import RootStackScreen from './RootStackScreen';
@@ -12,13 +10,10 @@ import Background from '../components/Background'
 const Stack = createStackNavigator();
 
 export default AppStack = () => {
-  
+
   const usersOnline = useSelector(state => state.usersOnline);
   console.log("usersOnline", usersOnline);
-  
-  const [loginState, dispatch] = useState(false);
-  const username = "";
-  // console.log(username);
+
   // if (isLoading) {
   //   return (
   //     <Background>
@@ -32,7 +27,7 @@ export default AppStack = () => {
     <NavigationContainer>
       {/* {loginState.userToken === null ? ( */}
       {username ?
-        <Stack.Navigator headerMode='none'>
+        <Stack.Navigator /* headerMode='none' */>
           <Stack.Screen name="ChatRoom" component={ChatRoom} />
           <Stack.Screen name="LobbyScreen" component={LobbyScreen} />
         </Stack.Navigator>
